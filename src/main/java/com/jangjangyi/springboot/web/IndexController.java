c07e234caa4584354f31ce8792de8cefaf70809a
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-04-21 17:21:37 +0900

    게시글 조회하는 페이지, 컨트롤러, 서비스 구현하기

--- src/main/java/com/jangjangyi/springboot/web/IndexController.java
+++ src/main/java/com/jangjangyi/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.jangjangyi.springboot.web;
 
+import com.jangjangyi.springboot.service.PostsService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
