0498c6849205fca73355d78deaa939bc25c33781
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-04-25 15:29:57 +0900

    어노테이션 기반으로 Session.class 개선하기

--- src/main/java/com/jangjangyi/springboot/web/IndexController.java
+++ src/main/java/com/jangjangyi/springboot/web/IndexController.java
@@ -1,15 +1,14 @@
 package com.jangjangyi.springboot.web;
 
+import com.jangjangyi.springboot.config.auth.LoginUser;
 import com.jangjangyi.springboot.config.auth.dto.SessionUser;
 import com.jangjangyi.springboot.service.PostsService;
 import com.jangjangyi.springboot.web.dto.PostsResponseDto;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -20,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
@@ -40,4 +38,5 @@ public class IndexController {
         model.addAttribute("post",dto);
         return "posts-update";
     }
+
 }
